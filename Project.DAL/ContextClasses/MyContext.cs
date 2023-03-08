5c95ad1a2111478ff375a6aecd15a366937ae79e
Barkın Atıcı <brknatici@hotmail.com>
2023-03-08 15:57:14 +0300

    My context e mapler eklendi

--- Project.DAL/ContextClasses/MyContext.cs
+++ Project.DAL/ContextClasses/MyContext.cs
@@ -25,6 +25,10 @@ namespace Project.DAL.ContextClasses
             modelBuilder.Configurations.Add(new ExtraMap());
             modelBuilder.Configurations.Add(new RoomMap());
             modelBuilder.Configurations.Add(new RoomTypeMap());
+            modelBuilder.Configurations.Add(new GuestMap());
+            modelBuilder.Configurations.Add(new ReservationGuestMap());
+            modelBuilder.Configurations.Add(new ReservationMap());
+            modelBuilder.Configurations.Add(new RoomReservationMap());
         }
 
         public DbSet<Order> Orders { get; set; }
@@ -35,6 +39,12 @@ namespace Project.DAL.ContextClasses
         public DbSet<RoomType> RoomTypes { get; set; }
         public DbSet<OrderExtra> OrderExtras { get; set; }
         public DbSet<EmployeeProfile> EmployeeProfiles { get; set; }
+        public DbSet<RoomReservation> RoomReservations { get; set; }
+        public DbSet<Reservation> Reservations { get; set; }
+        public DbSet<ReservationGuest> ReservationGuests { get; set; }
+        public DbSet<Guest> Guests { get; set; }
+
+
 
     }
 }
